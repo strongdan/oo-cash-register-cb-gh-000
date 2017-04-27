d0c2049846bb3c4c2c777edd8722a1fae086bf4f
Dan Strong <dangaston@gmail.com>
2017-04-27 17:28:04 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
 
+class CashRegister
+
+  def initialize(discount={})
+    @total = 0
+    @discount = discount
+  end
+
+  attr_accessor :total, :discount, :last_trans_amount
+
+  def add_item(title, price, quantity=nil)
+    previous = @total
+    if quantity != nil
+      @total += price * quantity
+    else
+      @total += price
+    end
+  end
+
+
+end
